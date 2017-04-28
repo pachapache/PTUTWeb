711ca67f2194c785f393880c98047f6e75ff4c77
Anaïs <chaumeilanais@gmail.com>
2017-04-28 16:18:22 +0200

    Page Chill Ok

--- PTUT/web/js/traitement_chill.js
+++ PTUT/web/js/traitement_chill.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  function randomGif(){
-    document.getElementById('BreatheWithTheShape').src = "./img/BreatheWithTheShape" + Math.round(Math.random()*4+1) + ".gif";
-  }
-}
+
+  document.getElementById('BreatheWithTheShape').src = "./img/BreatheWithTheShape" + Math.round(Math.random()*4+1) + ".gif";
+  console.log("hey");
+});
